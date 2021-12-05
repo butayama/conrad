ce42a348d5811b6c038696dc68e8d126ec0ada40
butayama <butayama@users.noreply.github.com>
2021-12-05 11:14:40 +0100

    add README_02.md and zahl_finden_rsp.py

---
+++ README_02.md
@@ -0,0 +1,4 @@
+# conrad 10 Experimente
+raspberry Pi verstehen und anwenden
+programme zur Anleitung:
+ISBN 978-3-645-10145-5
---
+++ zahl_finden_rsp.py
@@ -0,0 +1,19 @@
+import random
+
+
+def zufall():
+    zahl = random.randrange(1000)
+    tipp = 0
+    i = 0
+    while tipp != zahl:
+        tipp = int(input("Dein Tipp:"))
+        if zahl < tipp:
+            print("Die gesuchte Zahl ist kleiner als ", tipp)
+        if zahl > tipp:
+            print("Die gesuchte Zahl ist groesser als ", tipp)
+        i += 1
+    print("Du hast die Zahl beim ", i, ". Tipp erraten")
+
+
+if __name__ == "__main__":
+    zufall()
