58a5e88c36635b1882b8415509ddd3105a880bdd
butayama <butayama@users.noreply.github.com>
2021-12-12 19:40:20 +0100

    add LED1_remote.py

---
+++ python_examples/gpio/BUTTON-LED-VARIABLE_a.py
@@ -0,0 +1,20 @@
+from signal import pause
+from gpiozero import PWMLED, Button
+
+led = PWMLED(17)
+dty_cyc = 0.1
+button = Button(2)
+led.value = dty_cyc
+
+
+def incr_duty_cycle():
+    global dty_cyc
+    if dty_cyc == 0.8:
+        dty_cyc = 0.1
+    dty_cyc = dty_cyc * 2
+    led.value = dty_cyc
+
+
+button.when_pressed = incr_duty_cycle
+
+pause()
---
+++ python_examples/gpio/LED1_remote.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from gpiozero import LED
+from gpiozero.pins.pigpio import PiGPIOFactory
+from time import sleep
+factory = PiGPIOFactory(host='192.168.2.172')
+led = LED(17, pin_factory=factory)
+
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
